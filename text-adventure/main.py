7ef065c8563fcd3b554a24811e0942d3f41a4af9
thomchug <59849813+thomchug@users.noreply.github.com>
2020-02-05 20:07:21 -0500

    Updated working game for Project 2 Text Adventure

--- text-adventure/main.py
+++ text-adventure/main.py
@@ -5,8 +5,6 @@ assert sys.version_info >= (3,7), "This script requires at least Python 3.7"
 
 # The game and item description files (in the same folder as this script)
 game_file = 'game.json'
-item_file = 'items.json'
-
 
 # Load the contents of the files into the game and items dictionaries. You can largely ignore this
 # Sorry it's messy, I'm trying to account for any potential craziness with the file location
@@ -14,19 +12,53 @@ def load_files():
     try:
         __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
         with open(os.path.join(__location__, game_file)) as json_file: game = json.load(json_file)
-        with open(os.path.join(__location__, item_file)) as json_file: items = json.load(json_file)
-        return (game,items)
+        return game
     except:
         print("There was a problem reading either the game or item file.")
         os._exit(1)
 
+def render(game,current):
+    c = game[current]
+    print("You are at the " + c["name"])
+    print(c["desc"])
+    print("\nAvailable exits: ")
+    for e in c["exits"]:
+        print(e["exit"].lower())
+
+def get_input():
+    response = input("What do you want to do? ")
+    response = response.upper().strip()
+    return response
+
+def update(game, current,response):
+    c = game[current]
+    for e in c["exits"]:
+        if response == e["exit"]:
+            return e["target"]
+
+    return current
+
 # The main function for the game
 def main():
     current = 'ENTR'  # The starting location
-    end_game = ['END']  # Any of the end-game locations
+    end_game = ['TREASR']  # Any of the end-game locations
 
-    (game,items) = load_files()
+    game = load_files()
 
+    while True:
+        render(game,current)
+
+        if current in end_game:
+            break #break out of the while loop
+
+        response = get_input()
+
+        if response == "QUIT" or response == "Q":
+            break #break out of the while loop
+
+        current = update(game, current, response)
+
+    print("Thanks for playing!")
     # Add your code here
 
 # run the main function
